973d624595dd7b05d8f5947410faeb89591d2ae5
Fernando Sulzbach <f.sulzbach02@gmail.com>
2025-08-13 16:12:38 -0300

    Initial Commit

---
+++ APIChallenge/APIChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIChallengeApp.swift
+//  APIChallenge
+//
+//  Created by Fernando Sulzbach on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct APIChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIChallengeTests/APIChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  APIChallengeTests.swift
+//  APIChallengeTests
+//
+//  Created by Fernando Sulzbach on 13/08/25.
+//
+
+import Testing
+@testable import APIChallenge
+
+struct APIChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
