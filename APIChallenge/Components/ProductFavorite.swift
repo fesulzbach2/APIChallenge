ff350833ec6d637e51ebfa70f19fba8ef8b96cb9
Fernando Sulzbach <f.sulzbach02@gmail.com>
2025-08-13 19:49:42 -0300

    3 major components donegit add . '' !

--- APIChallenge/Components/ProductFavorite.swift
+++ APIChallenge/Components/ProductFavorite.swift
@@ -7,30 +7,33 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ProductFavorite: View {
+    
+    var category: Categories = .Beauty
+    var name: String = "Product name with two or more lines goes here"
+    var price: Double = 0.5
+    
     var body: some View {
         
-        HStack{
+        HStack(spacing: 16) {
             
-            Image(systemName: "bag")
+            category.image
+                .resizable()
                 .frame(width: 78, height: 78)
-                .background(
-                    RoundedRectangle(cornerRadius: 12)
-                        .foregroundStyle(.gray)
-                )
+                .clipShape(RoundedRectangle(cornerRadius: 12))
                 .frame(alignment: .leading)
             
             HStack(spacing: 16) {
                 VStack(alignment: .leading) {
-                    Text("Product name with two or more lines goes here")
+                    Text(name)
                         .fontWeight(.regular)
                         .font(.system(size: 13))
                         .frame(height: 36)
                     
                     
-                    Text("US$ 0.00")
-                        .fontWeight(.semibold)
-                        .font(.system(size: 17))
+                    Text("US$\(price.formatted(.number.precision(.fractionLength(2))))")
+                    .fontWeight(.semibold)
+                    .font(.system(size: 17))
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .frame(height: 62)
@@ -60,7 +63,8 @@ struct ContentView: View {
         .padding(.vertical, 8)
         .padding(.leading, 8)
         .padding(.trailing, 16)
-        .frame(width: 361, height: 94)
+        .frame(maxWidth: .infinity)
+        .frame(height: 94)
         .background(
             RoundedRectangle(cornerRadius: 16)
                 .foregroundStyle(.backgroundsSecondary)
@@ -70,5 +74,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ProductFavorite()
 }
---
+++ APIChallenge/Components/ProductOrder.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  APIChallenge
+//
+//  Created by Fernando Sulzbach on 13/08/25.
+//
+
+import SwiftUI
+
+struct ProductOrder: View {
+    
+    var category: Categories = .Beauty
+    var deliveryDate: String = "DELIVERY BY MONTH, 00"
+    var name: String = "Product name with two or more lines goes here"
+    var price: Double = 0.0
+    
+    var body: some View {
+        
+        HStack(spacing: 16) {
+            
+            category.image
+                .resizable()
+                .frame(width: 78, height: 78)
+                .frame(alignment: .leading)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                
+            HStack(spacing: 16) {
+                VStack(alignment: .leading, spacing: 4) {
+                    
+                    Text(deliveryDate)
+                        .fontWeight(.regular)
+                        .font(.system(size: 12))
+                        .foregroundStyle(.labelsSecondary)
+                    
+                    Text(name)
+                        .fontWeight(.regular)
+                        .font(.system(size: 13))
+                    
+                    
+                    Text("US$\(price.formatted(.number.precision(.fractionLength(2))))")
+                    .fontWeight(.semibold)
+                    .font(.system(size: 17))
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .frame(height: 62)
+                
+            }
+        }
+        .padding(.vertical, 8)
+        .padding(.leading, 8)
+        .padding(.trailing, 16)
+        .frame(maxWidth: .infinity)
+        .frame(height: 94)
+        .background(
+            RoundedRectangle(cornerRadius: 16)
+                .foregroundStyle(.backgroundsSecondary)
+        )
+        
+    }
+}
+
+#Preview {
+    ProductOrder()
+}
